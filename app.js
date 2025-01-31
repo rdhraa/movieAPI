e2db90cb56391e4737ac071ccda245ee3d96a299
Ardhra <ardhra367617@gmail.com>
2025-01-31 20:21:27 +0530

    route path changed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const filmsRouter = require('./routes/films');
 
 app.use(express.json());
-app.use('/films', filmsRouter);
+app.use('/movies', filmsRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
